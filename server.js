b43ce582a7a0025090128d9492649af48eced5e7
THT <tiago.h.terron@gmail.com>
2022-04-08 21:29:49 +0200

    server-update

--- server.js
+++ server.js
@@ -43,5 +43,5 @@ app.get('/getTransactionByHashFrom/:hash', routers)
 
 const PORT = process.env.PORT || 9000;
 app.listen(PORT, "0.0.0.0", () => {
-  console.log(`Server is running on port ${PORT}.`);
+  console.log(`Running on port ${PORT}.`);
 });
